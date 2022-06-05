0ec57d7f015a064fdd89884ec084e565c38b3d67
Kanat Tabaldiev <ktnaneri@gmail.com>
2022-06-05 23:27:29 +0600

    updated migrations

--- internal/services/RunMigrations.go
+++ internal/services/RunMigrations.go
@@ -1,13 +1,14 @@
 package services
 
 import (
+	"github.com/naneri/diploma/internal/item"
 	"github.com/naneri/diploma/internal/user"
 	"gorm.io/gorm"
 	"log"
 )
 
 func RunMigrations(db *gorm.DB) {
-	migrationErr := db.AutoMigrate(&user.User{})
+	migrationErr := db.AutoMigrate(&user.User{}, &item.Item{})
 
 	if migrationErr != nil {
 		log.Fatalf("error migrating: %s", migrationErr.Error())
